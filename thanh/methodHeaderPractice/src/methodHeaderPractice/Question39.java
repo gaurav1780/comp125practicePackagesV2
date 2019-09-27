6caac001c481c05803fb8ee2c1c5e5628b212f15
Duc Dung Vu <vuducdung98417@gmail.com>
2019-09-27 15:29:30 +1000

    Update Javadoc
    my copy error 👍. Sorry

--- thanh/methodHeaderPractice/src/methodHeaderPractice/Question39.java
+++ thanh/methodHeaderPractice/src/methodHeaderPractice/Question39.java
@@ -4,17 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Question39 {
-	/**
-	 * @param str
-	 * @return number of characters in str
-	 * NOTE: contains/indexOf/lastIndexOf is not allowed
-	 */
-	//Rewritten javadoc
 	/*
 	* @param str1, str2
 	* @return true if str1 contains str2
 	* @return false otherwise
-	* is the above NOTE still holds for this question? that is, can't students use those built in methods?
 	*/
 
 	@Test
