251c7810a142762cf403c0fdee47da3ceb267109
ncthanh2000 <53460339+ncthanh2000@users.noreply.github.com>
2019-09-27 14:26:50 +1000

    Fixed "AssertEqual" --> "AssertEquals", added null test

--- thanh/methodHeaderPractice/src/methodHeaderPractice/Question42.java
+++ thanh/methodHeaderPractice/src/methodHeaderPractice/Question42.java
@@ -7,14 +7,15 @@ public class Question42 {
 	/**
 	 * @param str
 	 * @return a string with letters only 
-	 * 			return null if str is null
+	 * return null if str is null
 	 */
 	@Test
 	void testGetLetter() {
-		assertEqual("Hello", getLetter("Hello"));
-		assertEqual("Thisiscompcourse", getLetter("This is comp 125 course!!"));
-		assertEqual("MynamesMrRobot", getLetter("My name's Mr.Robot"));
-		assertEqual("onetwofour", getLetter("one 1 two 2 four 5"));
-		assertEqual("", getLetter("\t \n"));		//it's really weird
+		assertNull(getLetter(null));
+		assertEquals("Hello", getLetter("Hello"));
+		assertEquals("Thisiscompcourse", getLetter("This is comp 125 course!!"));
+		assertEquals("MynamesMrRobot", getLetter("My name's Mr.Robot"));
+		assertEquals("onetwofour", getLetter("one 1 two 2 four 5"));
+		assertEquals("", getLetter("\t \n"));		//it's really weird
 	}
 }
