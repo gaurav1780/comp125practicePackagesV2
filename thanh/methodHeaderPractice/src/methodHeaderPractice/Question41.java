828fe7c12498f347fe5bb6d695c5049bd656737c
ncthanh2000 <53460339+ncthanh2000@users.noreply.github.com>
2019-09-27 14:23:44 +1000

    added non - null and empty assumption

--- thanh/methodHeaderPractice/src/methodHeaderPractice/Question41.java
+++ thanh/methodHeaderPractice/src/methodHeaderPractice/Question41.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Question41 {
 	
 	/**
+	 * assume not null and not empty
 	 * @param str1
 	 * @param str2
 	 * @return true if String str1 and String str2 are mutually reverse
